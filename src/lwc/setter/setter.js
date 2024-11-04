e61aed83ad6f8ae7bb80d447af98830dbd64fa02
Wouter <whtspc@gmail.com>
2024-11-04 17:17:58 +0100

    css gridDispl

--- src/lwc/setter/setter.js
+++ src/lwc/setter/setter.js
@@ -2,8 +2,16 @@
  * Created by vissewo on 04/11/2024.
  */
 
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 
-export default class Setter extends LightningElement {
+export default class ZeroSetter extends LightningElement {
+    connectedCallback() {
+        this.dispatchEvent(new FlowAttributeChangeEvent('outputValue', 0));
+    }
 
+    @api
+    get outputValue() {
+        return 0;
+    }
 }
